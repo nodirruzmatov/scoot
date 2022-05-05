ea5ccffd0aebd0aefd144b0ae685baac95fcb86b
nodirruzmatov <nodir7ruzmatov@gmail.com>
2022-05-05 10:50:59 +0300

    feat: close  menu when you press place  outside  hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+
+
 const elHamburgerBtn = document.querySelector(".header__hamburger");
+const elHamburgerBtnClose = document.querySelector(".header__bg");
 
 const elHeader = document.querySelector(".header");
 
-elHamburgerBtn.addEventListener("click", () => {
+elHamburgerBtn.addEventListener("click", function(){
   elHeader.classList.toggle("header--open")
+})
+
+elHamburgerBtnClose.addEventListener("click", function(){
+  elHeader.classList.remove("header--open")
 })
